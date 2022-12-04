6b73afa77cdace4e17b21b34ccf4f3872ce31dd6
salman-ahmed979 <salman.ahme979@hotmail.com>
2022-12-04 21:18:43 +0500

    Booking Controller added in FactoryController

--- backend/controller/factoryController.js
+++ backend/controller/factoryController.js
@@ -1,5 +1,6 @@
 const { TrainController } = require("../controller/trainController");
 const { UserController } = require("../controller/userController");
+const { BookingController } = require("./bookingController");
 
 class FactoryController {
   constructor() {}
@@ -10,6 +11,8 @@ class FactoryController {
       return new TrainController();
     } else if (controller.includes("usercontroller")) {
       return new UserController();
+    } else if(controller.includes("bookingcontroller")) {
+      return new BookingController();
     }
     return null;
   }
